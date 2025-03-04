e845a85e42988f96e381deb51df7e0c6825275f2
Fernando Moyano <fermoyano07@gmail.com>
2025-03-04 14:59:35 -0300

    Primer commit

---
+++ CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,84 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios objCapaDato = new CD_Usuarios();
+        public List<Usuario> Listar() { 
+            return objCapaDato.Listar();
+        }
+
+        public int Registrar(Usuario obj, out string Mensaje) {
+           
+            //Validar que lo campos no estén vacíos
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "El nombre del usuario no puede estar vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos)) 
+            {
+                Mensaje = "El apellido del usuario no puede estar vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "El correo del usuario no puede estar vacío";
+            }
+
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                string clave = "test123";
+                obj.Clave = CN_Recursos.ConvertirSha256(clave);
+
+                return objCapaDato.Registrar(obj, out Mensaje);
+
+            }
+            else {
+                return 0;
+            }
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            //Validar que lo campos no estén vacíos
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "El nombre del usuario no puede estar vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "El apellido del usuario no puede estar vacío";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "El correo del usuario no puede estar vacío";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(int id, out string Mensaje) 
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+    }
+}
